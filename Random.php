0eb6cf1071f72fcd85dcb9d519ff849ec4e2d7c7
GDenisC <92527725+GDenisC@users.noreply.github.com>
2022-05-03 16:26:34 +0700

    Random in PHP

--- Random.php
+++ Random.php
@@ -1,7 +1,56 @@
 <?php
-/*
- * biggest code of Random class and i deleted it :|
- * AND I AM DELETED THIS TWO TIMES
- * if you want create randon use "mt_rand()"
-*/
+class Random
+{
+    public function __constructor()
+    {
+        //
+    }
+    public function random()
+    {
+        return mt_rand() / mt_getrandmax();
+    }
+    public function range(float $min = 0, float $max = 1)
+    {
+        return $min + ($this->random() * ($max - $min));
+    }
+    public function irange(int $min = 0, int $max = 1)
+    {
+        return (int)($this->range($min, $max));
+    }
+    public function choice($array)
+    {
+        return $array[$this->irange(0, count($array))];
+    }
+    public function choices($array, int $length)
+    {
+        $rarray = [];
+        for ($i=0; $i < $length; $i++)
+        {
+            $rarray[$i] = $this->choice($array);
+        }
+        return $rarray;
+    }
+    public function strrange(int $length, string $addchars = "")
+    {
+        $rstr = "";
+        $rastr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".$addchars;
+        $_ = strlen($rastr);
+        for ($i=0; $i < $length; $i++)
+        {
+            $rstr .= $rastr[$this->irange(0, $_)];
+        }
+        return $rstr;
+    }
+    public function chance(float $chance = 50.0)
+    {
+        if ($this->range(0, 100) <= $chance)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
 ?>
